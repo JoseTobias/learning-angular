6ca865f680bf73898305c8d08c04402b3fe83290
JoseTobias <tobiassouza0@gmail.com>
2020-03-03 16:21:39 -0300

    learning routes and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { SecondComponent } from './second/second.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'first-component', component: DataBindingComponent },
+  { path: 'second-component', component: SecondComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
